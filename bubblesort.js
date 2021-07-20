3f1bb50b016fed67c43c05fe5de52590f752084d
kbalev <kamen_balev@yahoo.com>
2021-07-20 02:53:27 +0100

    bubblesort now works as intended

--- bubblesort.js
+++ bubblesort.js
@@ -2,31 +2,30 @@ let changes = 0;
 let pass = 0;
 
 const bubbleSort = (array) => {
-
-    for (let i = 0; i < array.length; i++) {
-        let first = array[i];
-        let second = array[i + 1]
+let bubbleSortArray = array
+    for (let i = 0; i < bubbleSortArray.length; i++) {
+        let first = bubbleSortArray[i];
+        let second = bubbleSortArray[i + 1]
         if (first > second) {
-            array.splice(i, 2, second, first)
-            console.log(`Swapped ${first} with ${second} and the array looks like this: ${array}`);
+            bubbleSortArray.splice(i, 2, second, first)
+            console.log(`Swapped ${first} with ${second} and the array looks like this: ${bubbleSortArray}`);
             changes++;
         } else {
-            console.log(`No change required, array looks like this: ${array}`)
+            console.log(`No change required, array looks like this: ${bubbleSortArray}`)
         }
     }
-    if (changes > 0) {
+    if (changes !== 0) {
         changes = 0
         pass++;
         console.log(`Array has gone through ${pass} iterations. Going for another one.`)
-        bubbleSort(array)
+        bubbleSort(bubbleSortArray)
     } else {
-        return array
+        console.log(bubbleSortArray)
     }
 }
 
 const numbers = [45, 98, 35, 76, 12, 4, 18, 5, 643, 34, 723, 21124, 10]
-const sortedNumbers = bubbleSort(numbers)
-console.log(sortedNumbers)
+bubbleSort(numbers)
 
 //fizz buzz
 //binary sort
